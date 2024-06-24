f441189cc20e378a253c8adf283e27cd39ce8149
2167706 <133228756+2167706@users.noreply.github.com>
2024-06-24 20:53:15 +0200

    stemmen.py

--- stemmen.py
+++ stemmen.py
@@ -1,18 +1,18 @@
 
-stemmen_omer = 0
-stemmen_mehmet = 0
+stemmen_younes = 0
+stemmen_lucas = 0
 
 print("Welkom bij de verkiezingen van Florijnia!")
-print("Je kunt stemmen op Omer of Mehmet.")
+print("Je kunt stemmen op younes of lucas.")
 print("Type 'UITSLAG' om de stemmen te stoppen en de winnaar te zien.\n")
 
 stem = input("Op wie wil je stemmen? ")
 
 while stem != 'UITSLAG':
 
-    if stem.lower() == 'omer':
+    if stem.lower() == 'younes':
         stemmen_omer += 1
-    elif stem.lower() == 'mehmet':
+    elif stem.lower() == 'lucas':
         stemmen_mehmet += 1
     else:
         print("Ongeldige keuze. Probeer opnieuw.")
@@ -20,9 +20,9 @@ while stem != 'UITSLAG':
     stem = input("Op wie wil je stemmen? ")
 
 # Bepalen van de winnaar op basis van het aantal stemmen
-if stemmen_omer > stemmen_mehmet:
+if stemmen_younes > stemmen_lucas:
     print("Omer heeft gewonnen!")
-elif stemmen_mehmet > stemmen_omer:
+elif stemmen_lucas > stemmen_younes:
     print("Mehmet heeft gewonnen!")
 else:
     print("Het is een gelijkspel!")
